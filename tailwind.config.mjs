cc6bdc6d0e9318d40423f130515e2075bde14993
Eilay K <22532056+eilayk@users.noreply.github.com>
2024-08-21 18:27:30 -0400

    create navbar

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				"bg": "#1a1a1a",
+				"primary": "#f0f0f0",
+				"secondary": "#ff6b6b",
+				"accent": "#ff6b6b",
+				"accent-hover": "#ff4f4f",
+				"accent-pressed": "#ff3333",
+				"surface": "#333333",
+				"text": "#f0f0f0",
+			}
+		},
+	},
+	plugins: [],
+}
