73d84c34f287de2ead21cf39a82254753fd3b8a0
Eilay Katsnelson <22532056+eilayk@users.noreply.github.com>
2026-09-01 13:52:27 -0400

    build: update outdated tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()]
+  integrations: [react(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
